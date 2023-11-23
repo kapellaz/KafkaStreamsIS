2460120688d1f71f4cc18963abe6af560ff64b09
kapellaz <rpcsantos192002@hotmail.com>
2023-11-23 17:48:33 +0000

    aadasda

--- src/main/java/org/example/KafkaConsumer.java
+++ src/main/java/org/example/KafkaConsumer.java
@@ -1,19 +1,39 @@
 package org.example;
+import org.apache.log4j.BasicConfigurator;
+import java.util.Properties;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
 
 import java.util.Properties;
 
 public class KafkaConsumer {
     public static void main(String[] args) {
-        // Create a Kafka Streams producer
-        Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092");
-        props.put("acks", "all"); // This is dangerous in production
-        props.put("retries", 0);
-        props.put("batch.size", 16384); // 16 KB
-        props.put("linger.ms", 1);
-        props.put("buffer.memory", 33554432); // 32 MB
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringSerializer");
+        BasicConfigurator.configure();
+        String topicName = "Buy";
+        String outtopicname = "results_topics";
+
+        java.util.Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "exercises-application");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "0.0.0.0:29092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.Long().getClass());
+
+        StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, Long> lines = builder.stream(topicName);
+
+        KTable<String, Long> outlines = lines.
+                groupByKey().count();
+        outlines.toStream().to(outtopicname);
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        streams.start();
+
+        System.out.println("Reading stream from topic " + topicName);
 
     }
 }

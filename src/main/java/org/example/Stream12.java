57625c70c0b670ed66d5482c26ccf1a168027b82
Bruno Sequeira <94460833+Sequeira12@users.noreply.github.com>
2023-12-01 16:40:45 +0000

    Nao sei o que se passa ;(

--- src/main/java/org/example/Stream12.java
+++ src/main/java/org/example/Stream12.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.apache.kafka.streams.kstream.*;
 import org.apache.log4j.BasicConfigurator;
+
 import java.util.Properties;
 import java.time.Duration;
 
@@ -16,8 +17,10 @@ import org.example.Serializer.Sale;
 public class Stream12 {
         public static void main(String[] args) {
                 BasicConfigurator.configure();
-                String topicName = "Sell";
-                String outtopicname = "req15";
+
+                String buyTopic = "testBuy1";
+                String sellTopic = "Sell";
+                String outTopic = "profitResult";
 
                 Properties props = new Properties();
                 props.put(StreamsConfig.APPLICATION_ID_CONFIG, "exercises-application10004");
@@ -25,17 +28,26 @@ public class Stream12 {
                 props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
                 props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, CustomSaleSerializer.class);
 
+                // SELL
                 StreamsBuilder builder = new StreamsBuilder();
-                KStream<String, Sale> lines = builder.stream(topicName, Consumed.with(
+
+                KStream<String, Sale> lines = builder.stream(sellTopic, Consumed.with(
                                 Serdes.String(),
                                 new CustomSaleSerializer()));
 
-                lines.foreach((key, value) -> System.out.println("key: " + key + " Value: " + value));
+                lines.foreach((key, value) -> System.out.println("key1: " + key + " Value: " + value));
+
+                KStream<String, Sale> lines2 = builder.stream(buyTopic, Consumed.with(
+                                Serdes.String(),
+                                new CustomSaleSerializer()));
+
+                lines2.foreach((key, value) -> System.out.println("key2: " + key + " Value: " + value));
 
                 // Get the total revenue in the last hour using a tumbling time window.
 
-                KTable<Windowed<String>, Double> out = lines
-                                .groupBy((key, value) -> value.getType())
+                KTable<Windowed<String>, Double> Sell = lines
+                                .groupBy((key, value) -> value.getType(),
+                                                Grouped.with(Serdes.String(), new CustomSaleSerializer()))
                                 .windowedBy(TimeWindows.of(Duration.ofSeconds(30)))
                                 .aggregate(
                                                 () -> 0.0,
@@ -43,17 +55,58 @@ public class Stream12 {
                                                                 + (newValue.getPricePerPair() * newValue.getQuantity()),
                                                 Materialized.with(Serdes.String(), Serdes.Double()))
                                 .toStream()
-                                .groupByKey() // Remova os parâmetros de tipo aqui
+                                .groupByKey()
                                 .reduce((aggValue, newValue) -> aggValue + newValue);
-                out.toStream().foreach(
-                                (key, value) -> System.out.println("Sock: " + key.key() + " Total Revenue: " + value));
 
-                out.toStream().to(outtopicname,
+                Sell.toStream().foreach(
+                                (key, value) -> System.out.println("Sock: " + key.key() + " Total Expenses: " + value));
+
+                Sell.toStream().to(outTopic,
                                 Produced.with(WindowedSerdes.timeWindowedSerdeFrom(String.class), Serdes.Double()));
 
+
+                //BUY
+                KTable<Windowed<String>, Double> BuyStream = lines2
+                                .groupBy((key, value) -> value.getType(),
+                                                Grouped.with(Serdes.String(), new CustomSaleSerializer()))
+                                .windowedBy(TimeWindows.of(Duration.ofSeconds(30)))
+                                .aggregate(
+                                                () -> 0.0,
+                                                (aggKey, newValue, aggValue) -> aggValue
+                                                                + (newValue.getPricePerPair() * newValue.getQuantity()),
+                                                Materialized.with(Serdes.String(), Serdes.Double()))
+                                .toStream()
+                                .groupByKey()
+                                .reduce((aggValue, newValue) -> aggValue + newValue);
+                BuyStream.toStream().foreach(
+                                (key, value) -> System.out.println("Sock: " + key.key() + " Total Revenue: " + value));
+
+                BuyStream.toStream().to(buyTopic,
+                                Produced.with(WindowedSerdes.timeWindowedSerdeFrom(String.class),
+                                                Serdes.Double()));
+                //JUNTAR
+                KTable<Windowed<String>, Double> profitTable = Sell
+                                .leftJoin(
+                                                BuyStream,
+                                                (sellValue, buyValue) -> {
+                                                        double sellAmount = (sellValue != null) ? sellValue : 0.0;
+                                                        double buyAmount = (buyValue != null) ? buyValue : 0.0;
+                                                        return buyAmount - sellAmount;
+                                                });
+
+                profitTable
+                                .toStream()
+                                .foreach((key, value) -> System.out
+                                                .println("Stock: " + key.key() + " Profit: " + value));
+
+                // Envie o resultado para o tópico de saída
+                profitTable.toStream().to(outTopic,
+                                Produced.with(WindowedSerdes.timeWindowedSerdeFrom(String.class),
+                                                Serdes.Double()));
                 KafkaStreams streams = new KafkaStreams(builder.build(), props);
                 streams.start();
 
-                System.out.println("Reading stream from topic " + topicName);
+                System.out.println("Reading streams from topics " + buyTopic + " and " + sellTopic);
+
         }
 }
